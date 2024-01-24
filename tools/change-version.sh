6c657b7fc478b040d30dd8388a865f0df1e28539
Robert Metzger <rmetzger@decodable.co>
2024-01-24 12:42:10 +0100

    DE-5804: Set version to 1.18.1-deco1

--- tools/change-version.sh
+++ tools/change-version.sh
@@ -17,8 +17,8 @@
 # limitations under the License.
 ################################################################################
 
-OLD="1.3-SNAPSHOT"
-NEW="1.4-SNAPSHOT"
+OLD="1.18-SNAPSHOT"
+NEW="1.18.1-deco1"
 
 
 HERE=` basename "$PWD"`
